3cb90adb7724ab122df19b9b6183281445db4126
Brian McVeigh <bmcveigh@forumone.com>
2019-03-21 16:42:51 -0400

    Added page contents component.

---
+++ src/components/PageContents/PageContents.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class PageContents extends React.Component {
+    public render() {
+        return (
+            <div>
+                Page Contents
+            </div>
+        );
+    }
+}
+
+export default PageContents;
