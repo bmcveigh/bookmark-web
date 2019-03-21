a512a5491d609b71a9f96da310cdd03a7ad8465f
Brian McVeigh <bmcveigh@forumone.com>
2019-03-21 16:32:05 -0400

    Use create-react-app instead.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
-import './sass/styles.scss';
-
-import './polyfills';
-
-render(
-  // Your app here
-  <div />,
-  document.getElementById('root'),
+ReactDOM.render(
+  <App />,
+  document.getElementById('root') as HTMLElement
 );
+registerServiceWorker();
--- src/polyfills.ts
+++
@@ -1,2 +0,0 @@
-// Ensure promises are globally available
-import 'es6-promise/auto';
