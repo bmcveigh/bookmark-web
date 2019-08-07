dd28fc986eae4c3f110e9f5310227fd881264cef
Brian McVeigh <bmcveigh@forumone.com>
2019-08-07 10:07:05 -0400

    Categories component route set to "/app".

--- src/components/PageContents/PageContents.tsx
+++ src/components/PageContents/PageContents.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Container} from 'reactstrap';
 
+import Categories from "../Categories/Categories";
 import Homepage from "../pages/Homepage/containers/Homepage/Homepage";
 
 function PageContents() {
@@ -10,9 +11,8 @@ function PageContents() {
         <Container fluid={true}>
             <h1>Bookmarks</h1>
             <Router>
-                <div>
-                    <Route path="/" component={Homepage}/>
-                </div>
+                <Route path="/" exact={true} component={Homepage} />
+                <Route path="/app" component={Categories} />
             </Router>
         </Container>
     );
