cb2ebcbd132bf7cce1ef400a5e237f156dd4a867
Brian McVeigh <bmcveigh@forumone.com>
2019-08-07 09:56:44 -0400

    Added Hompage component files.

--- src/components/pages/Homepage/containers/Homepage/Homepage.tsx
+++ src/components/pages/Homepage/containers/Homepage/Homepage.tsx
@@ -1,41 +1,45 @@
 import * as React from 'react';
 
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
-import { Col, Row } from 'reactstrap';
+import {Col, Row} from 'reactstrap';
 
-import * as classes './Homepage.css';
+import Benefits from '../../components/Benefits/Benefits';
+import Heading from '../../components/Heading/Heading';
+import Pricing from '../../components/Pricing/Pricing';
 
-import * as Benefits from '../../components/Benefits/Benefits';
-import * as Heading from '../../components/Heading/Heading';
-import * as Pricing from '../../components/Pricing/Pricing';
+function Homepage() {
+    const classes = require('./Homepage.css');
 
-function Homepage(props) {
-  return (
-    <div>
-      <Row className={classes.HomepageSection}>
-        <Col md={12}>
-          <Heading />
-        </Col>
-      </Row>
-      <Row>
-        <Col md={12} className={classes.HomepageSection}>
-          <Benefits />
-        </Col>
-      </Row>
-      <Row>
-        <Col md={12} className={classes.HomepageSection}>
-          <Pricing />
-        </Col>
-      </Row>
-    </div>
-  );
+    return (
+        <div>
+            <Row className={classes.HomepageSection}>
+                <Col md={12}>
+                    <Heading />
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12} className={classes.HomepageSection}>
+                    <Benefits />
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12} className={classes.HomepageSection}>
+                    <Pricing />
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
-function mapStateToProps(state) {
-  return {
-    user: state.user,
-  };
+interface IStateHomepage {
+    user: object;
+}
+
+function mapStateToProps(state: IStateHomepage) {
+    return {
+        user: state.user,
+    };
 }
 
 export default connect(mapStateToProps)(Homepage);
