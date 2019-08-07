b5c1d555b6f990b23394619a122af021575cc8a2
Brian McVeigh <bmcveigh@forumone.com>
2019-08-07 11:57:53 -0400

    Added in PageContents css file.

--- src/modules/PageContents/PageContents.tsx
+++ src/modules/PageContents/PageContents.tsx
@@ -6,9 +6,11 @@ import {Container} from 'reactstrap';
 import Categories from "../Bookmark/Categories";
 import Homepage from "../Homepage/containers/Homepage/Homepage";
 
+import './PageContents.css';
+
 function PageContents() {
     return (
-        <Container fluid={true}>
+        <Container fluid={true} className="page-contents">
             <h1>Bookmarks</h1>
             <Router>
                 <Route path="/" exact={true} component={Homepage} />
