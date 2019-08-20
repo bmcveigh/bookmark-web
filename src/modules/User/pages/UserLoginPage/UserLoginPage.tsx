10f159398b7f24eefed693b227333313792a9298
Brian McVeigh <bmcveigh@forumone.com>
2019-08-20 19:43:06 -0400

    styling for login page

--- src/modules/User/pages/UserLoginPage/UserLoginPage.tsx
+++ src/modules/User/pages/UserLoginPage/UserLoginPage.tsx
@@ -3,8 +3,11 @@ import {Col, Row} from "reactstrap";
 import UserLoginForm from "../../forms/UserLoginForm/UserLoginForm";
 
 export default function UserLoginPage() {
+    const classes = require(
+        "./UserLoginPage.scss"
+    );
     return (
-        <Row>
+        <Row className={ classes.UserLoginPage }>
             <Col md={6}>
                 <UserLoginForm />
             </Col>
